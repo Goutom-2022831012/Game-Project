79fb355164011b8c7ca4fa2cd202124ce7bd888e
Goutom Das Dipto <goutomdipto794@gmail.com>
2025-03-24 10:51:09 +0600

    setting up the project structur

---
+++ core/src/main/java/com/dipto/game/screen/GameScreen.java
@@ -0,0 +1,32 @@
+package com.dipto.game.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.dipto.game.MainGame;
+
+public class GameScreen implements Screen {
+    private MainGame game;
+    private OrthographicCamera camera;
+
+    public GameScreen(MainGame game) {
+        this.game = game;
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+    }
+
+    @Override
+    public void show() {}
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override public void resize(int width, int height) {}
+    @Override public void pause() {}
+    @Override public void resume() {}
+    @Override public void hide() {}
+    @Override public void dispose() {}
+}
